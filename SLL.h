23ed60440f1dc36aec91d4d3f18a5783ed1cde9f
Himanshu Yadav <639himanshuyadavg@gmail.com>
2024-10-25 01:39:37 +0530

    Created header file for SLL

---
+++ SLL.h
@@ -0,0 +1,31 @@
+#ifndef SLL_H
+#define SLL_H
+
+#include <vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node(int data1, Node *next1);
+    Node(int data1);
+};
+
+
+Node* convertArr2LL(vector<int> &arr);
+int lengthofLL(Node* head);
+int checkIfPresent(Node* head, int val);
+void print(Node* head);
+Node* removesHead(Node* head);
+Node* removesTail(Node* head);
+Node* removeK(Node* head, int k);
+Node* removeEl(Node* head, int el);
+Node* insertHead(Node* head, int val);
+Node* insertTail(Node* head, int val);
+Node* insertAtK(Node* head, int el, int k);
+Node* insertBeforeValue(Node* head, int el, int val);
+Node* reverseLL(Node* head);
+
+#endif
